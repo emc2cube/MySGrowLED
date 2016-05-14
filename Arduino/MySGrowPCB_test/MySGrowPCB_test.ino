a065b84b1282631dc4a30eeecf3681231898fb76
emc2cube <emc2cube+github@gmail.com>
2016-05-13 20:12:28 -0700

    BOM, test program
    Forgot BOM.
    Test program improved, added DS18b20 test.

--- Arduino/MySGrowPCB_test/MySGrowPCB_test.ino
+++ Arduino/MySGrowPCB_test/MySGrowPCB_test.ino
@@ -4,18 +4,26 @@
 // The pin the ATSHA204 is connected on
 #define ATSHA204_PIN 17   // A3
 
-#include <SPIFlash.h>     // From https://github.com/LowPowerLab/SPIFlash
+// OneWire pin
+#define ONE_WIRE_BUS 4    // D4
+
+#include <SPIFlash.h>     // From https://github.com/mysensors/Arduino/tree/development/libraries/SPIFlash as of May 2016 original from https://github.com/LowPowerLab/SPIFlash do not work with arduino 1.6.x
 #include <sha204_library.h>
 #include <sha204_lib_return_codes.h>
 #include <SPI.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 SPIFlash flash(FLASH_SS);
 const int sha204Pin = ATSHA204_PIN;
 atsha204Class sha204(sha204Pin);
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 void setup() {
   Serial.begin(115200);
-
+  sensors.begin();
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -33,25 +41,36 @@ void setup() {
   uint16_t crc;
 
   Serial.println(F("----------------------------------------------------"));
+  Serial.println(F("-------- MySGrowLED: optional hardware test --------"));
+  Serial.println(F("----------------------------------------------------"));
+  Serial.println("Dallas sensor");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  float temperature = sensors.getTempCByIndex(0);
+  if (temperature != -127.00 && temperature != 85.00) {
+    Serial.print("Current temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C");
+  } else {
+    Serial.println("Not found");
+  }
+  Serial.print(F("--------------------------"));
+
+  Serial.println(F("--------------------------"));
   Serial.println(F("ATSHA204"));
 
   // Wake device before starting operations
   ret_code = sha204.sha204c_wakeup(rx_buffer);
   if (ret_code != SHA204_SUCCESS)
   {
-    Serial.print(F("Failed to wake device. Response: ")); Serial.println(ret_code, HEX);
-    halt();
+    Serial.println(F("Not found"));
   }
-
-  // Output device revision on console
-  ret_code = sha204.sha204m_dev_rev(tx_buffer, rx_buffer);
-  if (ret_code != SHA204_SUCCESS)
-  {
-    Serial.print(F("Failed to determine device revision. Response: ")); Serial.println(ret_code, HEX);
-    halt();
-  }
-  else
-  {
+  else {
+    // Output device revision on console
+    ret_code = sha204.sha204m_dev_rev(tx_buffer, rx_buffer);
+    if (ret_code != SHA204_SUCCESS)
+    {
+      Serial.print(F("Failed to determine device revision. Response: ")); Serial.println(ret_code, HEX);
+    }
     Serial.print(F("Device revision: "));
     for (int i = 0; i < 4; i++)
     {
@@ -62,58 +81,61 @@ void setup() {
       Serial.print(rx_buffer[SHA204_BUFFER_POS_DATA + i], HEX);
     }
     Serial.println();
+
+    // Output serial number on console
+    ret_code = sha204.getSerialNumber(rx_buffer);
+    if (ret_code != SHA204_SUCCESS)
+    {
+      Serial.print(F("Failed to obtain device serial number. Response: ")); Serial.println(ret_code, HEX);
+    }
+    else
+    {
+      Serial.print(F("Device serial:   "));
+      for (int i = 0; i < 9; i++)
+      {
+        if (rx_buffer[i] < 0x10)
+        {
+          Serial.print('0'); // Because Serial.print does not 0-pad HEX
+        }
+        Serial.print(rx_buffer[i], HEX);
+      }
+      Serial.println();
+    }
   }
 
-  // Output serial number on console
-  ret_code = sha204.getSerialNumber(rx_buffer);
-  if (ret_code != SHA204_SUCCESS)
-  {
-    Serial.print(F("Failed to obtain device serial number. Response: ")); Serial.println(ret_code, HEX);
-    halt();
-  }
-  else
-  {
-    Serial.print(F("Device serial:   "));
-    for (int i = 0; i < 9; i++)
-    {
-      if (rx_buffer[i] < 0x10)
-      {
-        Serial.print('0'); // Because Serial.print does not 0-pad HEX
-      }
-      Serial.print(rx_buffer[i], HEX);
-    }
-    Serial.println();
-  }
   Serial.print(F("--------------------------"));
 
   Serial.println(F("--------------------------"));
   Serial.println(F("Eeprom"));
-  if (flash.initialize())
+  flash.initialize();
+  if (flash.readDeviceId() != 0)
   {
-    Serial.print(F("Eeprom DeviceID: "));
+    Serial.print(F("Device ID: "));
     Serial.println(flash.readDeviceId(), HEX);
   }
   else {
-    Serial.println(F("Not found."));
+    Serial.println(F("Not found"));
   }
   Serial.println(F("----------------------------------------------------"));
+  Serial.println(F(""));
+  Serial.println(F(""));
 
-  delay(1000);
+  delay(2500);
+
+  Serial.println(F("----------------------------------------------------"));
+  Serial.println(F("------------- MySGrowLED: mosfets test -------------"));
+  Serial.println(F("----------------------------------------------------"));
 }
 
 void loop() {
-  analogWrite(5, 0);
-  analogWrite(6, 255);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(10000);
+  Serial.println(F("LED: on - Fan: off - Onboard LED: on"));
   analogWrite(5, 255);
   analogWrite(6, 0);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(10000);
-}
-
-void halt()
-{
-  Serial.println(F("Halting!"));
-  while (1);
+  Serial.println(F("LED: off - Fan: on - Onboard LED: off"));
+  analogWrite(5, 0);
+  analogWrite(6, 255);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(10000);
 }
